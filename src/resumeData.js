d68ba0634c2e31219d7ab2dcb65ad8564c288da5
sanjida nawrin <sanjidanawrin123@gmail.com>
2020-07-07 19:09:34 -0400

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,104 @@
+let resumeData = {
+    "imagebaseurl":"https://https://github.com/sn-1/",
+    "name": "sanjida nawrin",
+    "role": "Full stack developer",
+    "linkedinId":"https://www.linkedin.com/in/sanjida-nawrin-4b754219b/",
+    "facebook":"https://www.facebook.com/nawrin.shanjidasharmin",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/sanjida-nawrin-4b754219b/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/sn-1",
+          "className":"fa fa-github"
+        },
+        
+      ],
+    "aboutme":"I'm a current suudent of UCONN full stack coding bootcamp.",
+    "address":"USA",
+    "website":"Website : In progress",
+    "education":[
+      {
+        "UniversityName":"Eastern University",
+        "specialization":"",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2011",
+        "Achievements":"Some Achievements"
+      } ,
+      {
+        "UniversityName":"UCONN",
+        "specialization":"Bootcamp",
+        "MonthOfPassing":"feb",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Home Depot",
+        "specialization":"Customer survice",
+        "MonthOfLeaving":"may",
+        "YearOfLeaving":"2017",
+        "Achievements":"Some Achievements"
+      },
+
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Java Script"
+      },
+      {
+        "skillname":"Node"
+      },
+      {
+        "skillname":"My sql"
+      },
+      {
+        "skillname":"MONGO"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"Full Stack App",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"Full Stack App",
+        "imgurl":"images/portfolio/corona.png"
+      },
+      {
+        "name":"project3",
+        "description":"Full Stack App",  
+        "imgurl":"images/portfolio/bl.png"
+      },
+      {
+        "name":"project4",
+        "description":"Full Stack App",
+        "imgurl":"images/portfolio/rain.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"",
+        "name":""
+      },
+    ]
+  }
+  
+  export default resumeData
